6e0b74004f5a64dc424e0c4742df31c4fe0f0642
Ron Hitchens <ron@ronsoft.com>
2014-01-16 22:57:37 +0000

    Fixup colors

--- test-code/js/test-result-chart.js
+++ test-code/js/test-result-chart.js
@@ -1,28 +1,28 @@
 
 $(function ()
 {
-    Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function(color) {
+    Highcharts.getOptions().colors = Highcharts.map(['#00cc00', '#cccc00', '#cc0000', '#aaaaaa'], function(color) {
         return {
             radialGradient: { cx: 0.5, cy: 0.3, r: 0.7 },
+
             stops: [
                 [0, color],
-                [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+                [1, Highcharts.Color(color).brighten(-0.4).get('rgb')] // darken
             ]
         };
     });
 
     $('#graph-container').highcharts({
 
-        colors: [ '#00cc00', '#cccc00', '#cc0000' ],
-
         chart: {
             type: 'pie',
             borderColor: '#66aaaa',
-            borderWidth: 1,
+            borderWidth: 3,
             backgroundColor: ''
         },
+
         title: {
-            text: 'RDFa Suite Test Results'
+            text: 'RDFa Test Suite Results'
         },
 
         plotOptions: {
@@ -38,10 +38,10 @@ $(function ()
             }
         },
 
-
         series: [{
             type: 'pie',
             name: 'Count'
-    }]
-});
+        }]
+    });
+
 });
